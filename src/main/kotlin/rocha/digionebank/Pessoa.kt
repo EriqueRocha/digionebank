833195f5ae3fae0a91715996642d728f39a006fe
EriqueRocha <eriquebit@gmail.com>
2022-07-02 22:20:33 -0300

    classe pessoa criada

--- src/main/kotlin/rocha/digionebank/Pessoa.kt
+++ src/main/kotlin/rocha/digionebank/Pessoa.kt
@@ -1,4 +1,12 @@
 package rocha.digionebank
 
 class Pessoa {
+    var nome: String = "Erique"
+    var cpf: String = "123.345.567-78"
+
+
 }
+ fun main(){
+     val eu = Pessoa()
+     println(eu.nome+" "+eu.cpf)
+ }
