5be72edb3f8adb352666606e76c8d9f69499a8a9
EriqueRocha <eriquebit@gmail.com>
2022-07-02 22:30:31 -0300

    inner class

--- src/main/kotlin/rocha/digionebank/Pessoa.kt
+++ src/main/kotlin/rocha/digionebank/Pessoa.kt
@@ -5,8 +5,16 @@ class Pessoa {
     var cpf: String = "123.345.567-78"
 
 
+    /*
+    inner class edereco{                 todo: classes dentro de classes são mais recomendadas para utilizar recursos externos, como APIs
+        var Rua: String = "teste rua"
+    }
+*/
+
 }
  fun main(){
      val eu = Pessoa()
      println(eu.nome+" "+eu.cpf)
+
+     //println(eu.edereco().Rua)
  }
